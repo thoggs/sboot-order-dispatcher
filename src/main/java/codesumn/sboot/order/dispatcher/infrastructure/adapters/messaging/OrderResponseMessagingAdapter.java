75794960178832d38643d911ded2c4cfb8356256
Thiago Rodrigues <thiagothogs@icloud.com>
2025-01-30 19:29:37 -0300

    refactor: reorder imports in OrderResponseMessagingAdapter
    - Reorganized imports to follow standard conventions and improve readability.
    - Grouped and aligned imports for better maintainability.

--- src/main/java/codesumn/sboot/order/dispatcher/infrastructure/adapters/messaging/OrderResponseMessagingAdapter.java
+++ src/main/java/codesumn/sboot/order/dispatcher/infrastructure/adapters/messaging/OrderResponseMessagingAdapter.java
@@ -4,13 +4,13 @@ import codesumn.sboot.order.dispatcher.application.dtos.records.order.OrderRecor
 import codesumn.sboot.order.dispatcher.domain.inbound.OrderProcessorPort;
 import codesumn.sboot.order.dispatcher.domain.inbound.OrderResponseMessagingPort;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-import org.springframework.amqp.support.AmqpHeaders;
-import com.rabbitmq.client.Channel;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
